50684d5f0bd61bcddd3073ae50712c991e6585c0
Mac Ignacio <mack_ignacio@yahoo.com>
2021-01-22 18:40:18 +0800

    FEAT: Replace Header with Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header";
+import Home from "./views/Home";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Home />
     </div>
   );
 }
