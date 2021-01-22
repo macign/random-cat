e14fa74c335771cc84a3ee6241c69a62c2c828a3
Mac Ignacio <mack_ignacio@yahoo.com>
2021-01-22 21:54:56 +0800

    FIX: Error when selecting back "Select breed" option

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Row, Form, Col } from "react-bootstrap";
+import { CatBreed } from "../views/Home";
 
 interface HeaderProps {
   onSelectBreed: Function;
-  breeds: string[];
+  breeds: CatBreed[];
 }
 
 export default function Header({ onSelectBreed, breeds }: HeaderProps) {
   const selectBreed = (data: any) => {
-    onSelectBreed(data.target.value);
+    onSelectBreed(data.target.value >= 0 ? breeds[data.target.value] : { id: "Select breed" });
   };
 
   return (
@@ -18,10 +19,12 @@ export default function Header({ onSelectBreed, breeds }: HeaderProps) {
         <Col md="3" sm="6">
           <Form.Group controlId="formGridState">
             <Form.Label>Breed</Form.Label>
-            <Form.Control as="select" defaultValue="Select breed" onChange={selectBreed}>
-              <option>Select breed</option>
-              {breeds.map((item) => (
-                <option key={item}>{item}</option>
+            <Form.Control as="select" onChange={selectBreed}>
+              <option value={-1}>Select breed</option>
+              {breeds.map((item, index) => (
+                <option key={item.id} value={index}>
+                  {item.name}
+                </option>
               ))}
             </Form.Control>
           </Form.Group>
