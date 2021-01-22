49d43d415eac5d407602b500f70a7b39fe227492
Mac Ignacio <mack_ignacio@yahoo.com>
2021-01-22 18:59:16 +0800

    FEAT: Add HeaderProps

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { Row, Form, Col } from "react-bootstrap";
 
-export default function Header() {
+interface HeaderProps {
+  onSelectBreed: Function;
+  breeds: string[];
+}
+
+export default function Header({ onSelectBreed, breeds }: HeaderProps) {
+  const selectBreed = (data: any) => {
+    onSelectBreed(data.target.value);
+  };
+
   return (
     <div>
       <h1>Cat Browser</h1>
@@ -9,8 +18,11 @@ export default function Header() {
         <Col md="3" sm="6">
           <Form.Group controlId="formGridState">
             <Form.Label>Breed</Form.Label>
-            <Form.Control as="select" defaultValue="Select breed">
+            <Form.Control as="select" defaultValue="Select breed" onChange={selectBreed}>
               <option>Select breed</option>
+              {breeds.map((item) => (
+                <option key={item}>{item}</option>
+              ))}
             </Form.Control>
           </Form.Group>
         </Col>
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,10 +3,15 @@ import { Container } from "react-bootstrap";
 import Header from "../components/Header";
 
 export default function Home() {
+  const breeds: string[] = [];
+  const onSelectBreed = (value: any) => {
+    console.log(value);
+  };
+
   return (
     <div className="pt-4">
       <Container>
-        <Header />
+        <Header onSelectBreed={onSelectBreed} breeds={breeds} />
       </Container>
     </div>
   );
