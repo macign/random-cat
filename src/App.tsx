3f9e60c1be2eab948dc004ae4276bdb07337ca9f
Mac Ignacio <mack_ignacio@yahoo.com>
2021-01-22 18:00:53 +0800

    FEAT: Add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Row, Container } from "react-bootstrap";
+
+export default function Header() {
+  return (
+    <div className="mt-2">
+      <Container>
+        <h1>Cat Browser</h1>
+        <Row></Row>
+      </Container>
+    </div>
+  );
+}
