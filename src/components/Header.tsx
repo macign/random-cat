689ee8928797425877279c8bce6cec127b86d482
Mac Ignacio <mack_ignacio@yahoo.com>
2021-01-22 18:39:52 +0800

    CHORE: Remove Container in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,20 @@
 import React from "react";
-import { Row, Container, Form, Col } from "react-bootstrap";
+import { Row, Form, Col } from "react-bootstrap";
 
 export default function Header() {
   return (
-    <div className="pt-4">
-      <Container>
-        <h1>Cat Browser</h1>
-        <Row>
-          <Col md="3" sm="6">
-            <Form.Group controlId="formGridState">
-              <Form.Label>Breed</Form.Label>
-              <Form.Control as="select" defaultValue="Select breed">
-                <option>Select breed</option>
-              </Form.Control>
-            </Form.Group>
-          </Col>
-        </Row>
-      </Container>
+    <div>
+      <h1>Cat Browser</h1>
+      <Row>
+        <Col md="3" sm="6">
+          <Form.Group controlId="formGridState">
+            <Form.Label>Breed</Form.Label>
+            <Form.Control as="select" defaultValue="Select breed">
+              <option>Select breed</option>
+            </Form.Control>
+          </Form.Group>
+        </Col>
+      </Row>
     </div>
   );
 }
