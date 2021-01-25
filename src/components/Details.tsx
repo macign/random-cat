da63c8960ed838f851a34179abac54d48483d55e
Mac Ignacio <mack_ignacio@yahoo.com>
2021-01-25 23:48:21 +0800

    FEAT: Add fetching of specific cat

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,24 +1,36 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { Button, Card } from "react-bootstrap";
-import { Cat } from "../views/Home";
+import { Link, useParams } from "react-router-dom";
+import api from "../services/axios";
 
-interface DetailsProps {
-  cat: Cat;
-  onBack: Function;
-}
+export default function Details({ match }: any) {
+  const { id }: any = useParams();
+  const [cat, setCat] = useState<any>(null);
 
-export default function Details({ cat, onBack }: DetailsProps) {
+  useEffect(() => {
+    api
+      .get(`https://api.thecatapi.com/v1/images/${id}`)
+      .then((res) => {
+        setCat(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [id]);
+
   return (
     <Card>
       <Card.Header>
-        <Button onClick={() => onBack()}>Back</Button>
+        <Link to={`/?breed=${cat?.breeds[0]?.id}`}>
+          <Button>Back</Button>
+        </Link>
       </Card.Header>
-      <Card.Img variant="top" src={cat.url} height="300" />
+      <Card.Img variant="top" src={cat?.url} height="400" />
       <Card.Body>
-        <Card.Title>{cat.breeds[0].name}</Card.Title>
-        <h5>Origin: {cat.breeds[0].origin}</h5>
-        <h6>{cat.breeds[0].temperament}</h6>
-        <Card.Text>{cat.breeds[0].description}</Card.Text>
+        <Card.Title>{cat?.breeds[0]?.name}</Card.Title>
+        <h5>Origin: {cat?.breeds[0]?.origin}</h5>
+        <h6>{cat?.breeds[0]?.temperament}</h6>
+        <Card.Text>{cat?.breeds[0]?.description}</Card.Text>
       </Card.Body>
     </Card>
   );
