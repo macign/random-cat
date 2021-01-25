15618a79cf9c5f0e51d55b143c12aaa488deae3b
Mac Ignacio <mack_ignacio@yahoo.com>
2021-01-25 23:48:37 +0800

    FIX: Rendering of breeds dropdown

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,35 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { Row, Form, Col } from "react-bootstrap";
+import { useHistory, useLocation } from "react-router-dom";
 import { CatBreed } from "../views/Home";
+import api from "../services/axios";
 
-interface HeaderProps {
-  onSelectBreed: Function;
-  breeds: CatBreed[];
-  defaultValue: number;
-}
+export default function Header() {
+  const [breeds, setBreeds] = useState<CatBreed[]>([]);
+  const { search } = useLocation();
 
-export default function Header({ onSelectBreed, breeds, defaultValue }: HeaderProps) {
+  const [breed, setBreed] = useState<number>(-1);
+  const history = useHistory();
+
   const selectBreed = (data: any) => {
-    onSelectBreed(data.target.value >= 0 ? { ...breeds[data.target.value], index: data.target.value } : { id: "Select breed", index: -1 });
+    const selectedBreed = breeds[data.target.value];
+    history.push(selectedBreed?.id ? `/?breed=${selectedBreed.id}` : "/");
+    setBreed(breeds.findIndex((e: any) => e.id === selectedBreed?.id));
   };
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(search);
+    const breedParam = searchParams.get("breed");
+    api
+      .get("breeds")
+      .then((res) => {
+        setBreeds(res.data);
+        setBreed(res.data.findIndex((e: any) => e.id === breedParam));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [search]);
 
   return (
     <div>
@@ -20,7 +38,7 @@ export default function Header({ onSelectBreed, breeds, defaultValue }: HeaderPr
         <Col md="3" sm="6">
           <Form.Group controlId="formGridState">
             <Form.Label>Breed</Form.Label>
-            <Form.Control as="select" defaultValue={defaultValue} onChange={selectBreed} disabled={breeds.length === 0}>
+            <Form.Control as="select" value={breed} onChange={selectBreed} disabled={breeds.length === 0}>
               <option value={-1}>Select breed</option>
               {breeds.map((item, index) => (
                 <option key={item.id} value={index}>
